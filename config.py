577813d5749bdbc95f122857523dab16210613e2
ThiraTheNerd <thiragithinji@gmail.com>
2021-06-13 16:21:00 +0300

    intstall sqlalchemy into our application

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    pass
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://thirathenerd:admin123@localhost/pitchtube'
 
 
 
