a058c2ebc72c230df0a3b03f5e4293e6eac8e44a
ThiraTheNerd <thiragithinji@gmail.com>
2021-06-17 11:02:02 +0300

    collect pitches data from the database

--- app/main/views.py
+++ app/main/views.py
@@ -58,7 +58,7 @@ def new_pitch():
   pitch_form = PitchForm()
   if pitch_form.validate_on_submit():
     pitch = Pitch(pitch=pitch_form.pitch_title.data, content =pitch_form.content.data, 
-    user_id = current_user.id,category = pitch_form.category.data,)
+    user = current_user.id,category = pitch_form.category.data,)
     db.session.add(pitch)
     db.session.commit()
     flash('Your pitch has been created successfully')
